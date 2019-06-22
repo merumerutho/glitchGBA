68753cb3c6fe2339b7205f7c8fee76b7a69e2ec9
Riccardo Straccia <merutochan@gmail.com>
2019-06-22 15:04:08 +0200

    Add files via upload
    First upload of sources. MAKEFILE is full of crap due to being integrated from another project. Nevermind. Coding style is also crap but the code is short, commented and overall very understandable.

---
+++ source/tile1.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(tile1)
+
+//======================================================================
+//
+//	tile1, 256x232@8, 
+//	+ palette 256 entries, not compressed
+//	+ 527 tiles (t|f|p reduced) not compressed
+//	+ regular map (flat), not compressed, 32x29 
+//	Total size: 512 + 33728 + 1856 = 36096
+//
+//	Time-stamp: 2019-06-21, 21:17:12
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_ROOM_H
+#define GRIT_ROOM_H
+
+#define tile1TilesLen 33728
+extern const unsigned short tile1Tiles[16864];
+
+#define tile1MapLen 1856
+extern const unsigned short tile1Map[928];
+
+#define tile1PalLen 512
+extern const unsigned short tile1Pal[256];
+
+#endif // GRIT_ROOM_H
+
+//}}BLOCK(tile1)
